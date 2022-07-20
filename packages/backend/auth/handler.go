81d58b66f680bef3a79c0024848806d0f884d339
Arthur Rubchenko <8872307@gmail.com>
2022-07-20 10:25:04 +0300

    refactor(backend): clean up auth

--- packages/backend/auth/handler.go
+++ packages/backend/auth/handler.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +19,34 @@ type authHandler struct {
 
 func NewAuthHandler(service AuthService) AuthHandler {
 	return &authHandler{service}
+}
+
+func (h *authHandler) loginWithGitHub(c *gin.Context) {
+	code := c.Query("code")
+	userData, err := h.service.getUserDataFromGitHub(code)
+	if err != nil {
+		// fmt.Println(err)
+		c.AbortWithStatusJSON(http.StatusForbidden, map[string]interface{}{
+			"message": "login failed",
+		})
+		return
+	}
+
+	// todo: login user
+	fmt.Println(userData)
+
+	c.Header("refresh-token", userData.Email)
+	c.Redirect(http.StatusFound, "http://localhost:8000/")
+}
+
+func (h *authHandler) redirectToGitHubLoginURL(c *gin.Context) {
+	q := url.Values{}
+	q.Set("client_id", os.Getenv("GITHUB_CLIENT_ID"))
+	q.Set("redirect_uri", ghRedirectURI)
+	q.Set("scope", ghUserEmailScope)
+	location := url.URL{Path: ghLoginOauthAuthorizeURI, RawQuery: q.Encode()}
+
+	c.Redirect(http.StatusFound, location.RequestURI())
 }
 
 func (h *authHandler) signIn(c *gin.Context) {
--- packages/backend/auth/service.go
+++ packages/backend/auth/service.go
@@ -1,8 +1,20 @@
 package auth
 
-import users "product-feedback/user"
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	users "product-feedback/user"
+	"product-feedback/utils"
+)
 
 type AuthService interface {
+	getGithubAccessToken(code string) (string, error)
+	getUserDataFromGitHub(code string) (userDataFromGitHub, error)
+	getUserEmailFromGitHub(accessToken string) (string, error)
 }
 
 type authService struct {
@@ -11,4 +23,139 @@ type authService struct {
 
 func NewAuthService(userRepo users.UserRepository) AuthService {
 	return &authService{userRepo}
+}
+
+const (
+	ghLoginOauthAccessTokenURI = "https://github.com/login/oauth/access_token"
+	ghLoginOauthAuthorizeURI   = "https://github.com/login/oauth/authorize"
+	ghRedirectURI              = "http://localhost:8000/api/auth/github/callback"
+	ghUserEmailsURI            = "https://api.github.com/user/emails"
+	ghUserEmailScope           = "user:email"
+)
+
+type ghAccessTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
+}
+
+func (s *authService) getGithubAccessToken(code string) (string, error) {
+	bodyMap := map[string]string{
+		"client_id":     os.Getenv("GITHUB_CLIENT_ID"),
+		"client_secret": os.Getenv("GITHUB_CLIENT_SECRET"),
+		"code":          code,
+	}
+	body := new(bytes.Buffer)
+	err := utils.ToJSON(body, bodyMap)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, ghLoginOauthAccessTokenURI, body)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	var ghResp ghAccessTokenResponse
+	err = utils.FromJSON(res.Body, &ghResp)
+	if err != nil {
+		return "", err
+	}
+
+	return ghResp.AccessToken, nil
+}
+
+type userDataFromGitHub struct {
+	AvatarUrl string `json:"avatar_url"`
+	Email     string `json:"email"`
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+}
+
+func (s *authService) getUserDataFromGitHub(
+	code string,
+) (userDataFromGitHub, error) {
+	accessToken, err := s.getGithubAccessToken(code)
+	if err != nil {
+		log.Println(err) // todo: use loggrus instance
+		return userDataFromGitHub{}, err
+	}
+
+	// avatarUrl: userGitHub.avatar_url,
+	// githubId: userGitHub.id,
+	// name: userGitHub.name
+
+	// email, err := h.getUserEmailFromGitHub(accessToken)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	c.AbortWithStatusJSON(http.StatusForbidden, map[string]interface{}{
+	// 		"message": "login failed",
+	// 	})
+	// 	return
+	// }
+
+	email, err := s.getUserEmailFromGitHub(accessToken)
+	if err != nil {
+		log.Println(err) // todo: use loggrus instance
+		return userDataFromGitHub{}, err
+	}
+
+	mockUserData := userDataFromGitHub{
+		Email: email,
+	}
+
+	return mockUserData, nil
+}
+
+type ghUserEmailResponse struct {
+	Email    string `json:"email"`
+	Primary  bool   `json:"primary"`
+	Verified bool   `json:"verified"`
+}
+
+func (s *authService) getUserEmailFromGitHub(
+	accessToken string,
+) (string, error) {
+	req, err := http.NewRequest(
+		http.MethodGet,
+		"https://api.github.com/user/emails",
+		nil,
+	)
+	if err != nil {
+		return "", nil
+	}
+
+	authTokenHeader := fmt.Sprintf("token %s", accessToken)
+	req.Header.Set("Authorization", authTokenHeader)
+	req.Header.Set("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", nil
+	}
+
+	// b, err := ioutil.ReadAll(res.Body)
+	// fmt.Println(string(b))
+
+	var emailsData []ghUserEmailResponse
+	err = utils.FromJSON(res.Body, &emailsData)
+	if err != nil {
+		return "", err
+	}
+
+	for _, data := range emailsData {
+		if data.Primary && data.Verified {
+			return data.Email, nil
+		}
+	}
+
+	return "", errors.New("could not retrieve an email")
 }
