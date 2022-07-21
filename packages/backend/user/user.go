38464896811fd93cfba8bd165532bbde36b1dafd
Arthur Rubchenko <8872307@gmail.com>
2022-07-21 15:31:25 +0300

    chore(backend): update users table

--- packages/backend/user/user.go
+++ packages/backend/user/user.go
@@ -3,11 +3,11 @@ package user
 // TODO: prepare migration or init User table in a proper way
 
 type User struct {
-	Id        int    `json:"id"`
-	Email     string `json:"email"`
-	Name      string `json:"name"`
-	UserName  string `json:"user_name"`
-	AvatarUrl string `json:"avatar_url"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	Id        int    `json:"id" db:"id"`
+	Email     string `json:"email" db:"email"`
+	Name      string `json:"name" db:"name"`
+	UserName  string `json:"user_name" db:"user_name"`
+	AvatarUrl string `json:"avatar_url" db:"avatar_url"`
+	CreatedAt string `json:"created_at" db:"created_at"`
+	UpdatedAt string `json:"updated_at" db:"updated_at"`
 }
